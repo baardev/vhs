75b6dfb2841490b3f758c355ac1616ef10f0ad88
tholonia <tholonia@mailfence.com>
2025-04-17 04:06:04 -0300

    25-04-17 04:06 porting]

--- backend/sql/06_create_default_users.sql
+++ backend/sql/06_create_default_users.sql
@@ -1,8 +1,6 @@
--- Create default users if they don't exist (passwd is "1q2w3e4r")
 INSERT INTO users (username, email, password)
 VALUES
 ('admin', 'admin@example.com', '$2b$10$rACnzk7TgV.rO.QBDxgE7.rjh5RMz0nG3XxQEPJ6waYZDEt7znmP.'),
 ('jm42', 'jeff.milton@gmail.com', '$2b$10$rACnzk7TgV.rO.QBDxgE7.rjh5RMz0nG3XxQEPJ6waYZDEt7znmP.'),
-('vhs', 'ns.victoria@gmail.com', '$2b$10$rACnzk7TgV.rO.QBDxgE7.rjh5RMz0nG3XxQEPJ6waYZDEt7znmP.'),
-ON CONFLICT (username) DO NOTHING;
-
+('vhs', 'ns.victoria@gmail.com', '$2b$10$rACnzk7TgV.rO.QBDxgE7.rjh5RMz0nG3XxQEPJ6waYZDEt7znmP.')
+ON CONFLICT (email) DO NOTHING;
